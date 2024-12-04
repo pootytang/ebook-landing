47b6ea32e72f861226ffc933c46e67d34e94ff09
pootytang <delane.jackson@gmail.com>
2024-12-04 14:02:58 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>  // import HeroSection from "$lib/components/HeroSection.svelte";
+  import { HeroSection, ChapterPreview, AuthorSection, FaqSection } from "$components";
+</script>
+
+<HeroSection />
+<ChapterPreview />
+<AuthorSection />
+<FaqSection />
